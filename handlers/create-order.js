e9c5f4abd8de589cd6f66baaacef45c2797a5e81
Doria, David <david.doria@intel.com>
2019-07-01 15:25:12 -0700

    Added initial DynamoDB integration

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,11 +1,31 @@
-function createOrder(order) {
-  if (!order || !order.pizzaId || !order.address) {
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+function createOrder(request) {
+  if (!request || !request.pizza || !request.address)
     throw new Error(
       "To order pizza please provide pizza type and address where pizza should be delivered"
     );
-  }
 
-  return {};
+  return docClient
+    .put({
+      TableName: "pizza-orders",
+      Item: {
+        orderId: "some-id",
+        pizza: request.pizza,
+        address: request.address,
+        orderStatus: "pending"
+      }
+    })
+    .promise()
+    .then(res => {
+      console.log("Order is saved!", res);
+      return res;
+    })
+    .catch(err => {
+      console.log(`Oops, order is not save :(`, error);
+      throw err;
+    });
 }
 
 module.exports = createOrder;
